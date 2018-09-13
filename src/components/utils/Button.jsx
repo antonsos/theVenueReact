94126e589269b661e95f8cc770ca1f10c55f98ce
Anton <mrservice.ip@gmail.com>
2018-09-13 13:39:45 +0700

    add Button component

---
+++ src/components/utils/Button.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+//material
+import Button from '@material-ui/core/Button'
+
+//icon
+import icon_ticket from '../../resources/images/icons/ticket.png';
+
+const MyButton = ({bgc, color, text, link}) => {
+  return (
+    <Button
+        href={link}
+        variant="contined"
+        size="small"
+        style={{
+            backgroundColor: bgc,
+            color: color
+        }}
+    >
+        <img
+            className="iconImage"
+            src={icon_ticket}
+            alt="icon"
+        />
+        {text}
+    </Button>
+  )
+}
+
+export default MyButton;
